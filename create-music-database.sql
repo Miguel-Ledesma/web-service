a7f3447e19376a86ae09dff606983e620f237188
Miguel-Ledesma <ledesmamiguel184@gmail.com>
2022-08-23 11:57:46 -0400

    password change

--- create-music-database.sql
+++ create-music-database.sql
@@ -2,5 +2,5 @@ DROP DATABASE IF EXISTS music;
 DROP USER IF EXISTS music_user@localhost;
 
 CREATE DATABASE music CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-CREATE USER music_user@localhost IDENTIFIED WITH mysql_native_password BY 'Amberpeake1!';
+CREATE USER music_user@localhost IDENTIFIED WITH mysql_native_password BY 'Amberpeake235!';
 GREANT ALL PRIVILEGES ON music.* TO music_user@localhost;
