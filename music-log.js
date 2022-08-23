82785879ec577842b9071adc8c59f494403f57a0
Miguel-Ledesma <ledesmamiguel184@gmail.com>
2022-08-23 15:42:33 -0400

    minor changes to fix

--- music-log.js
+++ music-log.js
@@ -1,8 +1,7 @@
 const express = require(express);
-const artistCounts = {};
 const service = express();
 const fs = require('fs');
-const request = require('https');
+const {request} = require('https');
 const json = fs.readFileSync('credentials.json', 'utf8');
 const mysql = require('mysql');
 const credentials = JSON.parse(json);
