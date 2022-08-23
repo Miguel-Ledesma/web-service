a01acd08d96dbe2c8230a11161239c794c5da663
Miguel-Ledesma <ledesmamiguel184@gmail.com>
2022-08-23 17:35:28 -0400

    re-re-revert

--- music-log.js
+++ music-log.js
@@ -230,7 +230,7 @@ service.delete('/artists/:id', (request, response) => {
 });
 
 // PORT THE PROGRAM IS ALIVE ON
-const port = 8445;
+const port = 8443;
 service.listen(port, () => {
     console.log(`I am alive on port ${port}!`);
 });
