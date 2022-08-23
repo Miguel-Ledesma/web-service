67de9971f38551da4551777ebbd4dcf28692cf61
Miguel-Ledesma <ledesmamiguel184@gmail.com>
2022-08-23 12:13:18 -0400

    create spelling error

--- create-music-database.sql
+++ create-music-database.sql
@@ -3,4 +3,4 @@ DROP USER IF EXISTS music_user@localhost;
 
 CREATE DATABASE music CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 CREATE USER music_user@localhost IDENTIFIED WITH mysql_native_password BY 'Amberpeake235!';
-GREANT ALL PRIVILEGES ON music.* TO music_user@localhost;
+GRANT ALL PRIVILEGES ON music.* TO music_user@localhost;
