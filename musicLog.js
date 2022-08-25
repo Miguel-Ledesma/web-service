47654fd6d142ac41c78b4b7247e01513c989c430
Miguel-Ledesma <ledesmamiguel184@gmail.com>
2022-08-25 15:23:16 -0400

    seeing if service was getting overridden by name search

--- musicLog.js
+++ musicLog.js
@@ -113,7 +113,7 @@ service.get('/artists/:artist/:album', (request, response) => {
 });
 
 // GET ALL ARTIST AND ALBUMS THAT CAME OUT IN A PARTICULAR YEAR
-service.get('/artists/:albumYear', (request, response) => {
+service.get('/albums/:albumYear', (request, response) => {
     const parameters = [request.params.albumYear];
     const query = 'SELECT artist, album, COUNT(song) FROM Music WHERE albumYear = ? GROUP BY artist, album ORDER BY artist';
 
